7460558c86cd0455adb1f0f7dfd1c068ff82122f
Payton Hansen1 <payton.hansen1@students.snow.edu>
2025-01-21 14:55:43 -0700

    messed around a little

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using Employees;
 
 FullTimeEmployee Steve = new FullTimeEmployee(1000.00m);
+Steve.Name = "Steve";
 
 PartTimeEmployee Fred = new PartTimeEmployee(10.00m, 160);
+Fred.Name = "Name";
 
 Console.WriteLine(Steve.CalculateSalary());
 Console.WriteLine(Fred.CalculateSalary());
